dfbc3994884ffb22346e98e09f9181002b3bb26e
Vicente Vega <vicente.vega@usach.cl>
2018-11-10 02:04:38 -0300

    add function printCurrent

--- Funcion.h
+++ Funcion.h
@@ -9,14 +9,12 @@ struct Caminos
 	struct Caminos* siguiente;
 	int camino[100];
 	int mejor[100];
-	//int costo;
 };
 typedef struct Caminos caminos;
 
 
 caminos* crearCamino(){
 	caminos* aux = (caminos*)malloc(sizeof(caminos));
-	//aux->camino = NULL;
 	aux->inicio = NULL;
 	aux->final = NULL;
 	aux->siguiente = NULL;
@@ -33,16 +31,36 @@ void ingresarCamino(caminos* lista,caminos* posibleCamino){
 	}
 }
 
-void ImprimirCaminos(caminos* lista){
-	caminos* aux = lista->inicio;
-	int i = 0;
-	while(aux!=NULL){
-		printf("%i %i %i %i %i\n",i+1,aux->camino[0],aux->camino[1],aux->camino[2],aux->camino[3]);
-		aux = aux->siguiente;
-		i++;
+/*
+void imprimirCamino(int camino[],int cantidadVertices){
+	for(int i=0;i<cantidadVertices;i++){
+		printf("%i ",camino[i] );
 	}
 	printf("\n");
+}*/
+
+int calcularCosto(int** matriz,int camino[],int cantidadVertices){
+	int costo = 0;
+	for(int i=0;i<cantidadVertices-1;i++){
+		costo = costo + matriz[camino[i]-1][camino[i+1]-1];
+	}
+	return costo;
 }
+
+
+void printCurrent(caminos* lista,int cantidadVertices,int** matriz){
+	#ifdef DEBUG
+	caminos* aux = lista->inicio;
+	while(aux!=NULL){
+		for(int i=0;i<cantidadVertices;i++){
+			printf("%i ",aux->camino[i] );
+		}
+		printf(" COSTO = %i\n",calcularCosto(matriz,aux->camino,cantidadVertices) );
+		aux = aux->siguiente;
+	}
+	#endif
+}
+
 
 int** formarMatriz(int tamano){
 	int** matriz = (int**)malloc(tamano*sizeof(int*));
@@ -56,47 +74,27 @@ int** formarMatriz(int tamano){
 	return matriz;
 }
 
-int calcularCosto(int** matriz,int camino[],int cantidadVertices){
-	int costo = 0;
-	printf("CAMINO = %i %i %i %i\n",camino[0],camino[1],camino[2],camino[3]);
-	for(int i=0;i<cantidadVertices-1;i++){
-		printf("%i %i %i\n",camino[i]-1,camino[i+1]-1,matriz[camino[i]-1][camino[i+1]-1]);
-		costo = costo + matriz[camino[i]-1][camino[i+1]-1];
-	}
-	return costo;
-}
 
 
 int mejorCamino(int** matriz,caminos* posibles,int cantidadVertices){
 	caminos* aux = posibles->inicio;
-	//ImprimirCaminos(posibles);
 	int mejor = 20000;
-	//int buenCamino[4];
 	while(aux != NULL){
 		int costo = calcularCosto(matriz,aux->camino,cantidadVertices) + 2;
-		printf("COSTO = %i\n",costo);
 		if(costo<mejor){
 			mejor = costo;
-			printf("COSTO 2= %i\n",costo);
 			memcpy(posibles->mejor,aux->camino,sizeof(posibles->mejor));
 		}
 		aux=aux->siguiente;
 	}
-	//printf("%i %i %i %i\n",buenCamino[0],buenCamino[1],buenCamino[2],buenCamino[3] );
 	return mejor;
 }
 
 void encontrarCamino(int vertice,int contador,int total,int lista[],int* visitados,caminos* posibles){
-	printf("/------------------------------------------------------/\n");
-	printf("VERTICE = %i\n",vertice );
-	printf("CONTADOR = %i\n",contador);
 	if(contador == total){
-		lista[contador-1] = vertice;
 		caminos* aux = crearCamino();
 		memcpy(aux->camino,lista,sizeof(aux->camino));
 		ingresarCamino(posibles,aux);
-		//printf("%i %i %i %i %i %i\n",lista[0],lista[1],lista[2],lista[3],lista[4],lista[5]);
-		printf("TERMINE\n");
 	}
 	else{
 		visitados[vertice-1] = 1;
@@ -114,13 +112,11 @@ void encontrarCamino(int vertice,int contador,int total,int lista[],int* visitad
 int** rellenarMatriz(char** info,int tamano){
 	int** matriz = formarMatriz(atoi(info[0]));
 	for(int i=1;i<tamano;i++){
-		printf("ENTRE CON = %i\n",i );
 		char* strFila = strtok(info[i]," ");
 		char* strColum = strtok(NULL," ");
 		char* strValor = strtok(NULL," ");
 		int fila = atoi(strFila)-1 ;
 		int columna = atoi(strColum)-1;
-		printf("%s %s\n",strFila,strColum);
 		matriz[fila][columna] = atoi(strValor);
 		matriz[columna][fila] = atoi(strValor);
 	}
@@ -134,7 +130,6 @@ void escribirArchivo(FILE* archivo,int mejor,caminos* posibles,int cantidadVerti
 		fprintf(archivo, "%i-",posibles->mejor[i]);
 	}
 	fprintf(archivo, "%i",0);
-	//fprintf(archivo, "%i %i %i %\n", );
 }
 
 
--- main.c
+++ main.c
@@ -4,19 +4,10 @@
 
 int main(){
 	char** info = (char**)malloc(1000*sizeof(char*));
-	int tamano = leerArchivo("input10.txt",info);
+	int tamano = leerArchivo("test4.txt",info);
 	printf("%i\n", tamano);
 	int** matrix2 = rellenarMatriz(info,tamano);
-	for(int i = 0;i<4;i++){
-		//printf("%i\n", matrix[i][0]);
-		for (int j = 0; j < 4; j++)
-		{
-			printf("%i ", matrix2[i][j]);
-		}
-		printf("\n");
-	}
 	int cantidadVertices = atoi(info[0]);
-	//int* num = (int*)malloc(cantidadVertices*sizeof(int));
 	int* num2 = (int*)malloc(cantidadVertices*sizeof(int));
 	int** prueba2 = (int**)malloc(6*sizeof(int*));
 	caminos* prueba = crearCamino();
@@ -26,10 +17,7 @@ int main(){
 		int num = 0;
 		encontrarCamino(i,1,cantidadVertices,test,num2,prueba);
 	}
-	//ImprimirCaminos(prueba);
-	int solucion[4];
 	int solucionValor = mejorCamino(matrix2,prueba,cantidadVertices);
-	printf("%i %i %i %i\n",prueba->mejor[0],prueba->mejor[1],prueba->mejor[2],prueba->mejor[3]);
 	FILE* nuevo = fopen("solucion","wt");
 	escribirArchivo(nuevo,solucionValor,prueba,cantidadVertices);
 	return 0;
